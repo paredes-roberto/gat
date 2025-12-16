63d316f0ac9bfd309b4a8e64275fa04edff7fe85
paredes-roberto <paredes.roberto.r@outlook.com>
2025-12-16 13:59:25 -0600

    Update main.js
    Update release 20251216 1359

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ const slidesData = [
             en: "Novahomes: turnkey construction and premium design with DCI methodology for total transparency and personalized experience."
         },
         //imageUrl: "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80"
-        imageUrl: "/img/sector1/06.webp"
+        imageUrl: "img/sector1/06.webp"
     },
     {
         id: "slide-background-2",
